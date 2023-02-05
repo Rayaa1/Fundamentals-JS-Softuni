08c682e41dad487c200ec6fbe140c03f19cd5f5c
Raya Koleva <117087939+Rayaa1@users.noreply.github.com>
2023-02-06 00:47:37 +0200

    Add files via upload

---
+++ 04.Functions - Exercise/07.NxNMatrix.js
@@ -0,0 +1,10 @@
+function NxNMatrix (num){
+    for(let i = 0 ; i < num ; i++ ){
+        let line = ""
+        for(let j = 0 ; j < num ; j++){
+            line += num + ' '
+        }
+        console.log(line)
+    }
+}
+NxNMatrix(7)
