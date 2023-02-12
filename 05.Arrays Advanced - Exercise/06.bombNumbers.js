3243a496ca8e063e96e4f28b5b4f88a87bb00c90
Raya Koleva <117087939+Rayaa1@users.noreply.github.com>
2023-02-13 00:25:21 +0200

    Add files via upload

---
+++ 05.Arrays Advanced - Exercise/06.bombNumbers.js
@@ -0,0 +1,21 @@
+function bombNumbers(arr, nums) {
+    let array = arr;
+    let bombNum = nums[0];
+    let power = nums[1];
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === bombNum) {
+            let firstIndex = i - power;
+            if(firstIndex < 0) {
+                firstIndex = 0;
+            } 
+            array.splice(firstIndex, (power * 2 + 1))
+            i = -1;
+        }
+    }
+    for (let j of array) {
+        sum += j; 
+    }
+    console.log(sum);
+}
+bombNumbers([1, 1, 2, 1, 1, 1, 2, 1, 1, 1],[2, 1])
