3aa6464db9c383203558e00d3206d0992b2e4e3b
Raya Koleva <117087939+Rayaa1@users.noreply.github.com>
2023-02-23 23:15:44 +0200

    Add files via upload

---
+++ 06.Objects and Classes - Lab/01.personInfo.js
@@ -0,0 +1,9 @@
+function personInfo(firstName,lastName,age){
+    let info = {
+        firstName : firstName,
+        lastName : lastName,
+        age : age,
+    }
+    return info
+}
+console.log("Peter" , "Pan","20")
