31ecd722485aa8e20792b7a753c54429f533fbec
Raya Koleva <117087939+Rayaa1@users.noreply.github.com>
2023-02-24 23:50:13 +0200

    Add files via upload

---
+++ 06.Objects and Classes - Lab/04.convertToJSON.js
@@ -0,0 +1,10 @@
+function convertToJson(name,lastName,hairColor){
+    let obj = {
+        name ,
+        lastName,
+        hairColor
+    }
+    console.log(JSON.stringify(obj))
+
+}
+convertToJson('George', 'Jones','Brown')
